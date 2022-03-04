734c4ce513b0e00c5432fae44b3d4caee3c98267
Matheus Comaru <matheuscomaru@gmail.com>
2022-03-03 22:03:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# LeitorNFe
+ Projeto Java para ler o XML da NF-e e gravar em classes para facilitar o uso em outras aplicações.
---
+++ src/com/comaru/leitornfe/teste.java
@@ -0,0 +1,11 @@
+package com.comaru.leitornfe;
+
+public class teste {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Olá");
+
+	}
+
+}
